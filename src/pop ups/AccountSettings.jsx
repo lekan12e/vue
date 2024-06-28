9a5d034f7482a9c702a95b76794838333a7fad9b
lekan <oyeniranolalekan89@gmail.com>
2024-06-28 13:21:54 +0100

    added page titlo

--- src/pop ups/AccountSettings.jsx
+++ src/pop ups/AccountSettings.jsx
@@ -4,7 +4,7 @@ import PersonIcon from '@mui/icons-material/Person';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import LogoutIcon from '@mui/icons-material/Logout';
 
-const AccountSettings = ({ setStudent, isPopUp }) => {
+const AccountSettings = ({ setStudent,isPopUp }) => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -14,18 +14,18 @@ const AccountSettings = ({ setStudent, isPopUp }) => {
   };
 
   return (
-    <div className={`absolute rounded-xl right-2 sm:right-8 top-12 sm:top-14 shadow-2xl bg-[#f7f8f8] justify-center transition-all duration-300 ease-in-out ${isPopUp ? "w-[200px] sm:w-[250px]" : "w-0 opacity-5 bg-emerald-600"}`}>
+    <div className={`absolute rounded-xl right-[8px] top-[49px] shadow-2xl bg-[#f7f8f8] justify-center transition-all duration-300 ease-in-out ${isPopUp? "w-[250px]" : "w-0 hidden opacity-5 bg-emerald-600"}`}>
       <Link className='cursor-pointer flex rounded-md mb-2 gap-2 w-full p-4 hover:bg-slate-300' to="/profile">
-        <PersonIcon sx={{ color: "#4B5543" }} />
-        <h1 className='font-poppins font-normal text-gray-600'>Profile</h1>
+        <PersonIcon sx={{color: "#4B5543"}} />
+        <h1 className='font-poppin font-normal text-gray-600'>Profile</h1>
       </Link>
       <div className='cursor-pointer flex rounded-md gap-2 mb-2 w-full p-4 justify-start items-center hover:bg-slate-300'>
-        <ManageAccountsIcon sx={{ color: "#4B5543" }} />
-        <h1 className='font-poppins font-normal text-gray-600'>Change Password</h1>
+        <ManageAccountsIcon sx={{color: "#4B5543"}} />
+        <h1 className='font-poppin font-normal text-gray-600'>Change Password</h1>
       </div>
       <div className='cursor-pointer flex rounded-md gap-2 w-full p-4 hover:bg-slate-300' onClick={handleLogout}>
-        <LogoutIcon sx={{ color: "#4B5543" }} />
-        <h1 className='font-poppins font-normal text-gray-600'>Logout</h1>
+        <LogoutIcon sx={{color: "#4B5543"}} />
+        <h1 className='font-poppin font-normal text-gray-600'>Logout</h1>
       </div>
     </div>
   );
